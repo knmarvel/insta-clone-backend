9ef515946c2db0bc5127d01b4937883d384d7171
AlvaradoM <mauriciomj11@gmail.com>
2020-07-10 04:38:09 -0700

    comments displaying

---
+++ comments/forms.py
@@ -0,0 +1,8 @@
+from comments.models import Comments
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('name', 'email', 'body')
