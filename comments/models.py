dfbf574bd245b7eaeea6aa9b47ff46cdd7d6f084
AlvaradoM <mauriciomj11@gmail.com>
2020-07-15 12:00:59 -0700

    comments showing up , adding works

--- comments/models.py
+++ comments/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
+from insta_backend.models import Post
 
 
 class Comments(models.Model):
-	comment_post = models.CharField(max_length=150)
-	author = models.ForeignKey('authentication.Author', related_name='commenter', on_delete=models.CASCADE)
-	commented_image = models.ForeignKey('insta_backend.Post', on_delete=models.CASCADE)
-	date = models.DateTimeField(auto_now_add=True)
+	posts = models.ForeignKey(Post, on_delete=models.CASCADE, null=True,
+	                          blank=True, related_name='comment_text')
+	author = models.ForeignKey('authentication.Author', max_length=200, on_delete=models.CASCADE)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved_comment = True
+		self.save()
 
 	def __str__(self):
-		return self.author.username
+		return self.text
